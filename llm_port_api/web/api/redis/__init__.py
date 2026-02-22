699ec42f21446e4bc2a06080c5434f969fa857ef
Sachi Harshitha Liyanagama <sachi.harshitha@live.com>
2026-02-22 16:20:42 +0100

    initial template project

---
+++ llm_port_api/web/api/redis/__init__.py
@@ -0,0 +1,5 @@
+"""Redis API."""
+
+from llm_port_api.web.api.redis.views import router
+
+__all__ = ["router"]
---
+++ llm_port_api/web/lifespan.py
@@ -0,0 +1,133 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from fastapi import FastAPI
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.aio_pika import AioPikaInstrumentor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.redis import RedisInstrumentor
+from opentelemetry.sdk.resources import (
+    DEPLOYMENT_ENVIRONMENT,
+    SERVICE_NAME,
+    TELEMETRY_SDK_LANGUAGE,
+    Resource,
+)
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import set_tracer_provider
+from prometheus_fastapi_instrumentator.instrumentation import (
+    PrometheusFastApiInstrumentator,
+)
+
+from llm_port_api.services.rabbit.lifespan import init_rabbit, shutdown_rabbit
+from llm_port_api.services.redis.lifespan import init_redis, shutdown_redis
+from llm_port_api.settings import settings
+from llm_port_api.tkq import broker
+
+
+def setup_opentelemetry(app: FastAPI) -> None:  # pragma: no cover
+    """
+    Enables opentelemetry instrumentation.
+
+    :param app: current application.
+    """
+    if not settings.opentelemetry_endpoint:
+        return
+
+    tracer_provider = TracerProvider(
+        resource=Resource(
+            attributes={
+                SERVICE_NAME: "llm_port_api",
+                TELEMETRY_SDK_LANGUAGE: "python",
+                DEPLOYMENT_ENVIRONMENT: settings.environment,
+            },
+        ),
+    )
+
+    tracer_provider.add_span_processor(
+        BatchSpanProcessor(
+            OTLPSpanExporter(
+                endpoint=settings.opentelemetry_endpoint,
+                insecure=True,
+            ),
+        ),
+    )
+
+    excluded_endpoints = [
+        app.url_path_for("health_check"),
+        app.url_path_for("openapi"),
+        app.url_path_for("swagger_ui_html"),
+        app.url_path_for("swagger_ui_redirect"),
+        app.url_path_for("redoc_html"),
+        "/metrics",
+    ]
+
+    FastAPIInstrumentor().instrument_app(
+        app,
+        tracer_provider=tracer_provider,
+        excluded_urls=",".join(excluded_endpoints),
+    )
+    RedisInstrumentor().instrument(
+        tracer_provider=tracer_provider,
+    )
+    AioPikaInstrumentor().instrument(
+        tracer_provider=tracer_provider,
+    )
+
+    set_tracer_provider(tracer_provider=tracer_provider)
+
+
+def stop_opentelemetry(app: FastAPI) -> None:  # pragma: no cover
+    """
+    Disables opentelemetry instrumentation.
+
+    :param app: current application.
+    """
+    if not settings.opentelemetry_endpoint:
+        return
+
+    FastAPIInstrumentor().uninstrument_app(app)
+    RedisInstrumentor().uninstrument()
+    AioPikaInstrumentor().uninstrument()
+
+
+def setup_prometheus(app: FastAPI) -> None:  # pragma: no cover
+    """
+    Enables prometheus integration.
+
+    :param app: current application.
+    """
+    PrometheusFastApiInstrumentator(should_group_status_codes=False).instrument(
+        app,
+    ).expose(app, should_gzip=True, name="prometheus_metrics")
+
+
+@asynccontextmanager
+async def lifespan_setup(
+    app: FastAPI,
+) -> AsyncGenerator[None, None]:  # pragma: no cover
+    """
+    Actions to run on application startup.
+
+    This function uses fastAPI app to store data
+    in the state, such as db_engine.
+
+    :param app: the fastAPI application.
+    :return: function that actually performs actions.
+    """
+
+    app.middleware_stack = None
+    if not broker.is_worker_process:
+        await broker.startup()
+    setup_opentelemetry(app)
+    init_redis(app)
+    init_rabbit(app)
+    setup_prometheus(app)
+    app.middleware_stack = app.build_middleware_stack()
+
+    yield
+    if not broker.is_worker_process:
+        await broker.shutdown()
+    await shutdown_redis(app)
+    await shutdown_rabbit(app)
+    stop_opentelemetry(app)
